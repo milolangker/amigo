0f5c98fb72d05e9ee689700e1fd63cad809aa7ba
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2025-10-14 11:50:59 +0200

    Moving AzimulthalAverage fn from drpangloss to Amigo, and removing the dependency. Resolves #55

--- src/amigo/vis_analysis.py
+++ src/amigo/vis_analysis.py
@@ -8,8 +8,8 @@ import optimistix as optx
 from jax import jit, vmap
 import dLux.utils as dlu
 from amigo.misc import interp
-from drpangloss.grid_fit import azimuthalAverage
 
+# from drpangloss.grid_fit import azimuthalAverage
 # from numpyro.distributions.util import gammaincinv
 from amigo.stats import orthogonalise
 from copy import deepcopy
@@ -431,3 +431,130 @@ def generate_photon_data(vis_eig_vals, n_terms, n_phot, cal_vis_dict, key=jr.key
     phot_cal_vis_dict["O_phi_mat"] = o_phi_mat
 
     return phot_cal_vis_dict
+
+
+"""
+Note the code below is a direct copy from the drpangloss repo. It has been placed here 
+becuase it is not a pip-installable package and therefore requires installing from 
+source every time amigo is installed. Given it is a large package with notebooks
+directly inside of the repo, this can be quite slow and vulnerable to breaking if the
+code changes on the main branch. 
+
+https://github.com/benjaminpope/drpangloss/blob/main/src/drpangloss/grid_fit.py#L269
+
+"""
+
+
+def azimuthalAverage(
+    image,
+    center=None,
+    stddev=False,
+    returnradii=False,
+    return_nr=False,
+    binsize=0.5,
+    weights=None,
+    steps=False,
+    interpnan=False,
+    left=None,
+    right=None,
+    return_max=False,
+):
+    """
+    Calculate the azimuthally-averaged radial profile.
+    NB: This was found online and should be properly credited! Modified by MJI
+
+    image - The 2D image
+    center - The [x,y] pixel coordinates used as the center. The default is
+             None, which then uses the center of the image (including
+             fractional pixels).
+    stddev - if specified, return the azimuthal standard deviation instead of the average
+    returnradii - if specified, return (radii_array,radial_profile)
+    return_nr   - if specified, return number of pixels per radius *and* radius
+    binsize - size of the averaging bin.  Can lead to strange results if
+        non-binsize factors are used to specify the center and the binsize is
+        too large
+    weights - can do a weighted average instead of a simple average if this keyword parameter
+        is set.  weights.shape must = image.shape.  weighted stddev is undefined, so don't
+        set weights and stddev.
+    steps - if specified, will return a double-length bin array and radial
+        profile so you can plot a step-form radial profile (which more accurately
+        represents what's going on)
+    interpnan - Interpolate over NAN values, i.e. bins where there is no data?
+        left,right - passed to interpnan; they set the extrapolated values
+    return_max - (MJI) Return the maximum index.
+
+    If a bin contains NO DATA, it will have a NAN value because of the
+    divide-by-sum-of-weights component.  I think this is a useful way to denote
+    lack of data, but users let me know if an alternative is prefered...
+
+    """
+    # Calculate the indices from the image
+    y, x = np.indices(image.shape)
+
+    if center is None:
+        center = np.array([(x.max() - x.min()) / 2.0, (y.max() - y.min()) / 2.0])
+
+    r = np.hypot(x - center[0], y - center[1])
+
+    if weights is None:
+        weights = np.ones(image.shape)
+    elif stddev:
+        raise ValueError("Weighted standard deviation is not defined.")
+
+    # the 'bins' as initially defined are lower/upper bounds for each bin
+    # so that values will be in [lower,upper)
+    nbins = int(np.round(r.max() / binsize) + 1)
+    maxbin = nbins * binsize
+    bins = np.linspace(0, maxbin, nbins + 1)
+    # but we're probably more interested in the bin centers than their left or right sides...
+    bin_centers = (bins[1:] + bins[:-1]) / 2.0
+
+    # Find out which radial bin each point in the map belongs to
+    whichbin = np.digitize(r.flatten(), bins)
+
+    # how many per bin (i.e., histogram)?
+    # there are never any in bin 0, because the lowest index returned by digitize is 1
+    nr = np.bincount(whichbin)[1:]
+
+    # recall that bins are from 1 to nbins (which is expressed in array terms by arange(nbins)+1 or
+    #  xrange(1,nbins+1)) radial_prof.shape = bin_centers.shape
+
+    if stddev:
+        radial_prof = np.array([image.flatten()[whichbin == b].std() for b in range(1, nbins + 1)])
+    elif return_max:
+        radial_prof = np.array(
+            [
+                np.append((image * weights).flatten()[whichbin == b], -np.inf).max()
+                for b in range(1, nbins + 1)
+            ]
+        )
+    else:
+        radial_prof = np.array(
+            [
+                (image * weights).flatten()[whichbin == b].sum()
+                / weights.flatten()[whichbin == b].sum()
+                for b in range(1, nbins + 1)
+            ]
+        )
+
+    # import pdb; pdb.set_trace()
+
+    if interpnan:
+        radial_prof = np.interp(
+            bin_centers,
+            bin_centers[radial_prof == radial_prof],
+            radial_prof[radial_prof == radial_prof],
+            left=left,
+            right=right,
+        )
+
+    if steps:
+        xarr = np.array(zip(bins[:-1], bins[1:])).ravel()
+        yarr = np.array(zip(radial_prof, radial_prof)).ravel()
+        return xarr, yarr
+    elif returnradii:
+        return bin_centers, radial_prof
+    elif return_nr:
+        return nr, bin_centers, radial_prof
+    else:
+        return radial_prof
